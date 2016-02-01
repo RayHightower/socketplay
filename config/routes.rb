b5ec197486d534404fdf637ed941312fd51e7fee
Raymond T. Hightower <rth@wisdomgroup.com>
2016-01-31 20:13:40 -0600

    create model for messages. gotta have a place to store them

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'rooms#show'
 
   resources :boards
-  root 'boards#index'
+  # root 'boards#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109032622) do
+ActiveRecord::Schema.define(version: 20160201021245) do
 
   create_table "boards", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
