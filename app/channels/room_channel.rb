581ca286bdd237d5821786470f8b46d0b5585345
Raymond T. Hightower <rth@wisdomgroup.com>
2016-02-02 14:56:53 -0600

    stream from room_channel, not some_channel. gotta get the channel names right!!!

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in an EventMachine loop that does not support auto reloading.
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "some_channel"
+    stream_from "room_channel"
   end
 
   def unsubscribed
