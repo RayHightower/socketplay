e20c94c7bc14a087bcbfa827d6c6c1cfe4078850
Pat Jones <patjones@example.com>
2016-02-21 03:26:38 -0600

    specify tzinfo explicitly in environment.rb. move toward resolving the parallella time zone problem

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Fix `rescue in find_zone!': Invalid Timezone: UTC (ArgumentError)
+require 'tzinfo'
+
 # Initialize the Rails application.
 Rails.application.initialize!
