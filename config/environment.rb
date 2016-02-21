4d6324916bc6ca9cd45ebbef373c43fdd8a8a451
Pat Jones <patjones@example.com>
2016-02-21 03:32:15 -0600

    require time zone info at the very beginning of the environment config file

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
-# Load the Rails application.
-require File.expand_path('../application', __FILE__)
-
 # Fix `rescue in find_zone!': Invalid Timezone: UTC (ArgumentError)
 require 'tzinfo'
+
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
